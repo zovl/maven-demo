54150a9b96af77d486582aebdf5df30356591bee
zovl <849075906@qq.com>
2017-10-08 17:18:27 +0800

    2017年 10月 08日 星期日 17:18:16 CST

---
+++ maven-jar/src/main/java/zovlzhongguanhua/demo/maven/JavaMain.java
@@ -0,0 +1,8 @@
+package zovlzhongguanhua.demo.maven;
+
+public class JavaMain {
+
+    public static void main(String[] args) {
+        System.out.println("------------------------------------->");
+    }
+}
